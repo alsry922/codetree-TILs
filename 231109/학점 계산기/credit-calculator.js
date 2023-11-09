365fa30398e866a7aec27b9e4546b4c561acbe9b
Jeong Mingyo <alsry922@gmail.com>
2023-11-09 23:54:54 +0900

    Title: 학점 계산기, Time: 229ms, Memory: 45MB, Status: Passed - CodeTree

---
+++ 231109/학점 계산기/credit-calculator.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const numOfSubject = Number.parseInt(input[0]);
+const scores = input[1].split(' ').map((ele) => Number.parseFloat(ele));
+const sumOfScores = scores.reduce((acc, curr) => {
+    return acc+curr;
+})
+const average = (sumOfScores / numOfSubject).toFixed(1);
+
+console.log(average);
+
+if (average >= 4.0) {
+    console.log('Perfect');
+} else if (average >= 3.0) {
+    console.log('Good');
+} else {
+    console.log('Poor');
+}
