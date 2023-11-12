f406efb98cf896c51bf8cae4664a1b99757fc96f
Jeong Mingyo <alsry922@gmail.com>
2023-11-12 22:45:22 +0900

    Title: 배열의 특정 위치 찾기 3, Time: 303ms, Memory: 13MB, Status: Passed - CodeTree

--- 231112/배열의 특정 위치 찾기 3/find-specific-location-fo-array-3.js
+++ 231112/배열의 특정 위치 찾기 3/find-specific-location-fo-array-3.js
@@ -2,5 +2,5 @@ const fs = require('fs');
 const arr = fs.readFileSync(0).toString().trim().split(' ').map(ele => Number.parseInt(ele));
 const zeroIdx = arr.findIndex(ele => ele === 0);
 
-const sum3 = arr.slice(zeroIdx-2, zeroIdx+1).reduce((acc, curr) => acc + curr);
+const sum3 = arr.slice(zeroIdx-3, zeroIdx).reduce((acc, curr) => acc + curr);
 console.log(sum3);
