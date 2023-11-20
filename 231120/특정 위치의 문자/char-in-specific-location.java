cced108dc4d793eef230ce07d82dcd1b04f15874
Jeong Mingyo <alsry922@gmail.com>
2023-11-20 22:54:23 +0900

    Title: 특정 위치의 문자, Time: 85ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 231120/특정 위치의 문자/char-in-specific-location.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char input = br.readLine().charAt(0);
+        Character[] characters = new Character[] {'L', 'E', 'B', 'R', 'O', 'S'};
+        int index = Arrays.asList(characters).indexOf(input);
+        System.out.println(index);
+    }
+}
