b29d0b8f71603e9451dd46c2e8224989127a345d
Jeong Mingyo <alsry922@gmail.com>
2023-11-21 10:17:53 +0900

    Title: 개수 세기, Time: 210ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 231121/개수 세기/count-numbers.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException, NumberFormatException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        long count = Arrays.stream(arr).filter(ele -> ele == m).count();
+        System.out.println(count);
+    }
+}
