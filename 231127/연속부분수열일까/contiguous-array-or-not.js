da8433bedf453140d4ce8846546a18eee2a5817d
Jeong Mingyo <alsry922@gmail.com>
2023-11-27 21:55:29 +0900

    Title: 연속부분수열일까, Time: 295ms, Memory: 13MB, Status: Passed - CodeTree

--- 231127/연속부분수열일까/contiguous-array-or-not.js
+++ 231127/연속부분수열일까/contiguous-array-or-not.js
@@ -5,12 +5,19 @@ const [n1, n2] = input[0].split(' ').map(ele => Number.parseInt(ele));
 const A = input[1].split(' ').map(ele => Number.parseInt(ele));
 const B = input[2].split(' ').map(ele => Number.parseInt(ele));
 
-const firstEleIdx = A.findIndex(ele => ele === B[0]);
-let answer = 'Yes';
-B.forEach((ele, index) => {
-    if (ele !== A[firstEleIdx + index]) {
-        answer = 'No';
+let answer = 'No';
+for (let AIdx = 0; AIdx < A.length; AIdx++) {
+    let index = AIdx;
+    let count = 0;
+    for (let BIdx = 0; BIdx < B.length; BIdx++) {
+        if (A[index] !== B[BIdx]) break;
+        index++;
+        count++;
     }
-});
+    if (count === B.length) {
+        answer = 'Yes';
+        break;
+    }
+}
 
 console.log(answer);
