b7e18fb78e1a03a579cde31fedb7d849b7343259
Jeong Mingyo <alsry922@gmail.com>
2024-02-03 20:13:16 +0900

    Title: k번째로 신기한 문자열, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240203/k번째로 신기한 문자열/kth-special-string.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static int inputCount, orderNumber;
+    public static String startString;
+    public static List<String> words = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] inputs = br.readLine().split(" ");
+        inputCount = Integer.parseInt(inputs[0]);
+        orderNumber = Integer.parseInt(inputs[1]);
+        startString = inputs[2];
+        for (int i = 0; i < inputCount; i++) {
+            String word = br.readLine();
+            if (word.startsWith("ap")) {
+                words.add(word);
+            }
+        }
+        Collections.sort(words);
+        System.out.println(words.get(orderNumber-1));
+        br.close();
+    }
+}
