61bf415c3735ff1cf163bf634c2060f625553425
Jeong Mingyo <alsry922@gmail.com>
2024-02-05 06:36:46 +0900

    Title: 줄 세우기, Time: 258ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240205/줄 세우기/line-up-students.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+class Student implements Comparable<Student> {
+    int number, height, weight;
+    public Student(int number, int height, int weight) {
+        this.number = number;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    @Override
+    public int compareTo(Student student) {
+        if (this.height != student.height) {
+            return student.height - this.height;
+        }
+        if (this.weight != student.weight) {
+            return student.weight - this.weight;
+        }
+        return this.number - student.weight;
+    }
+
+    public void print() {
+        System.out.println(String.format("%d %d %d", this.height, this.weight, this.number));
+    }
+}
+
+public class Main {
+    public static int inputLength;
+    public static List<Student> students = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        inputLength = Integer.parseInt(br.readLine());
+        for (int i = 0; i < inputLength; i++) {
+            int[] inputs = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            int number = i + 1;
+            int height = inputs[0];
+            int weight = inputs[1];
+            students.add(new Student(number, height, weight));
+        }
+
+        Collections.sort(students);
+        for (Student student : students) {
+            student.print();
+        }
+    }
+}
