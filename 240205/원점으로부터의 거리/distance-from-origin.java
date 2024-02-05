a44ec7d4a7248b0708cd70d3c48afe598ab02021
Jeong Mingyo <alsry922@gmail.com>
2024-02-05 13:57:17 +0900

    Title: 원점으로부터의 거리, Time: 280ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240205/원점으로부터의 거리/distance-from-origin.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+class Point implements Comparable<Point> {
+    int number, x, y;
+    public Point(int number, int x, int y) {
+        this.number = number;
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public int compareTo(Point point) {
+        if (Math.abs(this.x + this.y) == Math.abs(point.x + point.y)) {
+            return this.number - point.number;
+        }
+        return Math.abs(this.x + this.y) - Math.abs(point.x + point.y);
+    }
+}
+
+public class Main {
+    public static int inputLength;
+    public static List<Point> points = new ArrayList<>();
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        inputLength = Integer.parseInt(br.readLine());
+        for (int i = 0; i < inputLength; i++) {
+            int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            int number = i + 1;
+            int x = input[0];
+            int y = input[1];
+            points.add(new Point(number, x, y));
+        }
+        Collections.sort(points);
+        points.stream().forEach(point -> System.out.println(point.number));
+    }
+}
