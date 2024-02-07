09f31b54926061a565adfcaa3e1bb3ae5c7cb634
Jeong Mingyo <alsry922@gmail.com>
2024-02-08 06:48:07 +0900

    Title: 최대로 겹치는 구간, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240208/최대로 겹치는 구간/maximum-overlapped-segments.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+
+public class Main {
+    public static int OFFSET = 100;
+    public static int lineCount;
+    public static int[] overlap = new int[201];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        lineCount = Integer.parseInt(br.readLine());
+        for (int i = 0; i < lineCount; i++) {
+            int[] input = Arrays.stream(br.readLine().trim().split(" "))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+            int start = input[0];
+            int end = input[1];
+            for (int index = start; index < end; index++) {
+                overlap[index]++;
+            }
+        }
+        int answer = Arrays.stream(overlap).max().orElse(0);
+        System.out.println(answer);
+        br.close();
+    }
+}
