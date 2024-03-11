e57f8c47bcb911726dc2f2d6fa492080460e1dfa
Jeong Mingyo <alsry922@gmail.com>
2024-03-11 13:29:56 +0900

    Title: 작은 구슬의 이동, Time: 120ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240311/작은 구슬의 이동/small-marble-movement.java
+++ 240311/작은 구슬의 이동/small-marble-movement.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class Main {
     public static int N, T, DIR_NUM = 3;
-    public static int[] dirRow = new int[] {0, -1, 1, 0};
+    public static int[] dirRow = new int[] {0, 1, -1, 0};
     public static int[] dirCol = new int[] {1, 0, 0, -1};
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
