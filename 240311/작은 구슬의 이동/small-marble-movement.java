e1e119aa221764e87fb67139191be1dd432732d1
Jeong Mingyo <alsry922@gmail.com>
2024-03-11 13:28:36 +0900

    Title: 작은 구슬의 이동, Time: 131ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240311/작은 구슬의 이동/small-marble-movement.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static int N, T, DIR_NUM = 3;
+    public static int[] dirRow = new int[] {0, -1, 1, 0};
+    public static int[] dirCol = new int[] {1, 0, 0, -1};
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        T = sc.nextInt();
+        int r = sc.nextInt();
+        int c = sc.nextInt();
+        int d = getDirection(sc.next());
+
+        while (T-- > 0) {
+            int nextRow = r + dirRow[d];
+            int nextCol = c + dirCol[d];
+            if (!isInRange(nextRow, nextCol)) {
+                d = DIR_NUM - 3;
+            } else {
+                r = nextRow;
+                c = nextCol;
+            }
+        }
+        System.out.println(String.format("%d %d", r, c));
+    }
+
+    public static int getDirection(String dir) {
+        if (dir.equals("D")) 
+            return 0;
+        else if (dir.equals("S"))
+            return 1;
+        else if (dir.equals("N"))
+            return 2;
+        else
+            return 3;
+    }
+
+    public static boolean isInRange(int row, int col) {
+        return 1 <= row && row <= N && 1 <= col && col <= N;
+    }
+}
