6b0abef042c42f19dff16176dc8a7c4a4b4a909a
Jeong Mingyo <alsry922@gmail.com>
2024-03-11 12:08:24 +0900

    Title: 문자에 따른 명령 2, Time: 411ms, Memory: 15MB, Status: Passed - CodeTree

--- 240311/문자에 따른 명령 2/text-based-commands2.java
+++ 240311/문자에 따른 명령 2/text-based-commands2.java
@@ -5,24 +5,22 @@ public class Main {
     public static int dirNum = 3;
     public static int[] dx = new int[] {1, 0, -1, 0};
     public static int[] dy = new int[] {0, -1, 0, 1};
+    public static int x = 0, y = 0;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        for (int i = 0; i < N; i++) {
-            String[] input = sc.next().split();
-            for (int j = 0; j < input.length; j++) {
-                if (input[j].equals("R"))
-                    dirNum = (dirNum + 1) % 4;
-                else if (input[j].equals("L"))
-                    dirNum = (dirNum + 3) % 4;
-                else {
-                    x += dx[dirNum];
-                    y += dy[dirNum];
-                }    
 
-            }
+        String[] input = sc.next().split("");
+        for (int j = 0; j < input.length; j++) {
+            if (input[j].equals("R"))
+                dirNum = (dirNum + 1) % 4;
+            else if (input[j].equals("L"))
+                dirNum = (dirNum + 3) % 4;
+            else {
+                x += dx[dirNum];
+                y += dy[dirNum];
+            }    
+
         }
         System.out.println(String.format("%d %d", x, y));
-
     }
 }
