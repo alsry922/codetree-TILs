98d02b82ef8f466bc41640c59b508d5c5bcfccd8
Jeong Mingyo <alsry922@gmail.com>
2024-03-17 18:19:49 +0900

    Title: 국영수 순이지, Time: 103ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240317/국영수 순이지/korean-english-math-order.py
@@ -0,0 +1,16 @@
+class Student:
+    def __init__(self, name, kor, eng, math):
+        self.name = name
+        self.kor = kor
+        self.eng = eng
+        self.math = math
+
+n = int(input())
+students = [
+    Student(name, int(kor), int(eng), int(math)) for _ in range(n)
+        for name, kor, eng, math in [input().split()]
+]
+
+students.sort(key=lambda ele: (-ele.kor, -ele.eng, -ele.math))
+for ele in students:
+    print(ele.name, ele.kor, ele.eng, ele.math)
