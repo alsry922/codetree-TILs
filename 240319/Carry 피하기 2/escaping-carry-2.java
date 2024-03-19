bd920a27746817d4f953a5f6d9d4b91ba5972e73
Jeong Mingyo <alsry922@gmail.com>
2024-03-19 13:48:33 +0900

    Title: Carry 피하기 2, Time: 429ms, Memory: 16MB, Status: Passed - CodeTree

--- 240319/Carry 피하기 2/escaping-carry-2.java
+++ 240319/Carry 피하기 2/escaping-carry-2.java
@@ -17,7 +17,7 @@ public class Main {
             }
         }
 
-        int maxValue = 0;
+        int maxValue = -1;
         for (int i = 0; i < N-2; i++) {
             for (int j = i+1; j < N; j++) {
                 for (int k = j+1; k < N; k++) {
