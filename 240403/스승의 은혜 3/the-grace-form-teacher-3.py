7458b81264b12250c009877f6f2b9e4d5a912435
Jeong Mingyo <alsry922@gmail.com>
2024-04-03 15:10:56 +0900

    Title: 스승의 은혜 3, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240403/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -0,0 +1,22 @@
+N, B = tuple(map(int, input().split()))
+presents = [
+    tuple(map(int, input().split())) for _ in range(N)
+]
+
+max_count = 0
+
+for i in range(len(presents)):
+    discounted_presents = [
+        (present[0] // 2, present[1]) if index == i else present for index, present in enumerate(presents)
+    ]
+    discounted_presents.sort(key=lambda present: (present[0], present[1]))
+    sum_cost = 0
+    count = 0
+    for j in range(len(discounted_presents)):
+        if (sum_cost + discounted_presents[j][0] > B):
+            break
+        sum_cost += discounted_presents[j][0]
+        count += 1
+    max_count = max(max_count, count)
+
+print(max_count)
