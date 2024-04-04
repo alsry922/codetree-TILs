a8c8619272952c59e1f1fbbf3148264632b6c2de
Jeong Mingyo <alsry922@gmail.com>
2024-04-04 18:05:10 +0900

    Title: 빙산의 일각 2, Time: 172ms, Memory: 34MB, Status: Passed - CodeTree

--- 240404/빙산의 일각 2/the-tip-of-the-iceberg-2.py
+++ 240404/빙산의 일각 2/the-tip-of-the-iceberg-2.py
@@ -1,32 +1,21 @@
-MAX_H = 1000
-
-# 변수 선언 및 입력
-n = int(input())
-
-h = [
-    int(input())
-    for _ in range(n)
+N = int(input())
+glacier_height = [
+    int(input()) for _ in range(N)
 ]
 
-ans = 0
+answer = 0
 
-# 각 높이에 대해
-# 빙산 덩어리의 개수의 최댓값을 구합니다.
-for i in range(1, MAX_H):
-	# 물의 높이가 i일때 빙산 덩어리의 개수를 구합니다.
-    cnt = 0
+for ocean_height in range(1, 1001):
+    sink_glacier_heights = [
+        height - ocean_height for height in glacier_height
+    ]
+    count = 0
+    if sink_glacier_heights[0] > 0:
+        count += 1
+    for i in range(1, len(sink_glacier_heights)):
+        if sink_glacier_heights[i] > 0 and sink_glacier_heights[i - 1] <= 0:
+            count += 1
 
-    # 가장 왼쪽에 빙산이 있는 경우의 예외를 처리해줍니다.
-    if h[0] > i:
-        cnt += 1
+    answer = max(answer, count)
 
-    # 바로 앞 블록은 해수면에 잠겨있고
-    # 자기 자신의 블록은 해수면 위에 떠있는 경우,
-    # 자기 자신 블록부터 시작하는 빙산이 하나 더 있습니다.
-    for j in range(1, n):
-        if h[j] > i and h[j - 1] <= i:
-            cnt += 1
-
-    ans = max(ans, cnt)
-    
-print(ans)
+print(answer)
