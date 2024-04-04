83e55e7cafa8806257045a7f1ba69ee4460c8a0f
Jeong Mingyo <alsry922@gmail.com>
2024-04-04 17:37:41 +0900

    Title: 빙산의 일각 2, Time: 122ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240404/빙산의 일각 2/the-tip-of-the-iceberg-2.py
@@ -0,0 +1,23 @@
+N = int(input())
+glacier_height = [
+    int(input()) for _ in range(N)
+]
+
+answer = 0
+
+for ocean_height in range(1, 1001):
+    sink_glacier_heights = [
+        height - ocean_height for height in glacier_height
+    ]
+    count = 1
+    for index, height in enumerate(sink_glacier_heights):
+        if index == 0:
+            continue
+        
+        if height > 0 and sink_glacier_heights[index-1] <= 0:
+            count += 1
+        # elif height <= 0 and sink_glacier_heights[index-1] > 0:
+        #     count += 1
+    answer = max(answer, count)
+
+print(answer)
